a616dd5bd7e77453f5824fd7f0dd06ae250f9b51
Tayna0202 <taytv22@gmail.com>
2024-02-13 14:57:17 -0300

    front upgrade

---
+++ Back-End/script.js
@@ -0,0 +1,43 @@
+const formulario = document.querySelector('form')
+const Inome = document.querySelector('.nome')
+const Iemail = document.querySelector('.email')
+const Isenha = document.querySelector('.senha')
+const Iconfirmarsenha = document.querySelector('.confirmarsenha')
+
+// Conexão com banco de dados para o cadastro
+
+function cadastrar (){
+        fetch("http://localhost:8080/cadastrar",
+        {
+            headers: {
+                'Accept': 'aplication/json',
+                'Content-Type': 'aplication/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                nome: Inome.value,
+                email: Iemail.value,
+                senha: Isenha.value,
+                confirmasenha: Iconfirmarsenha.value
+            })
+        })
+        .then(function(res){ console.log(res) })
+        .catch(function(res){ console.log(res) })
+}
+
+// Função para limpar os dados preenchidos
+
+function limpar (){
+    Inome.value = "";
+    Iemail.value = "";
+    Isenha.value = "";
+    Iconfirmarsenha.value = "";
+};
+
+formulario.addEventListener('submit', function (event){
+    event.preventDefault();
+    
+    cadastrar();
+    limpar();
+
+});
--- script_trilho.js
+++
@@ -1,9 +0,0 @@
-let trilho = document.getElementById('trilho')
-let box = document.querySelector('.box')
-let button = document.querySelector('button')
-
-trilho.addEventListener('click', ()=>{
-    trilho.classList.toggle('dark')
-    box.classList.toggle('dark')
-    button.classList.toggle('dark')
-})
